6c19746cc8339787604b4459cbf06163443c8691
KantaHasegawa <gyoza20000@gmail.com>
2022-03-12 16:14:59 +0900

    feat: 確認後トリガー追加

---
+++ cognitoPostConfirmation/handler.js
@@ -0,0 +1,23 @@
+var aws = require('aws-sdk');
+
+exports.handler = async (event, context, callback) => {
+  const documentClient = new aws.DynamoDB.DocumentClient({});
+  console.log("function start")
+  if (event.request.userAttributes.sub) {
+    console.log("event.request.userAttributes.sub exists")
+    const params = {
+      user: event.request.userAttributes.sub
+    }
+    try {
+      await documentClient.put({ TableName: process.env.TABLE_NAME, Item: params }).promise()
+      console.log("put success")
+    } catch (error) {
+      console.log(error)
+      console.log("error!!")
+    }
+    callback(null, event);
+  } else {
+    console.log("not exists")
+    callback(null, event);
+  }
+};
