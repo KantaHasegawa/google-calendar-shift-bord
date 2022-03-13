cf0d74ecd0520c1f1013f196f0164747625c275e
KantaHasegawa <gyoza20000@gmail.com>
2022-03-14 01:09:25 +0900

    feat: Add spot data

--- src/entity/spot.go
+++ src/entity/spot.go
@@ -1,9 +1,17 @@
 package entity
 
+
+
 type TSpot struct {
     User      string    `json:"User"`
     StartWork   string `json:"StartWork"`
 		SpotId			string `json:"SpotId"`
+		SpotData struct {
+			Name string `json:"Name"`
+			Salary int `json:"Salaly"`
+			CutOffDay string `json:"CutOffDay"`
+			PayDay string `json:"PayDay"`
+	}
 }
 
 type SpotInteractorInteface interface {
