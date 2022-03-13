ce4ff247926015e9e9c5130f99775ce6c60dfd9f
KantaHasegawa <gyoza20000@gmail.com>
2022-03-13 22:25:03 +0900

    feat: add database

---
+++ src/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+func NewDatabaseClient(env string) *dynamodb.Client {
+	if(env == "dev"){
+		return createLocalClient()
+	}else{
+		return createLocalClient()
+	}
+}
+
+func createLocalClient() *dynamodb.Client {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-east-1"),
+		config.WithEndpointResolver(aws.EndpointResolverFunc(
+			func(service, region string) (aws.Endpoint, error) {
+				return aws.Endpoint{URL: "http://localhost:8000"}, nil
+			})),
+		config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
+			Value: aws.Credentials{
+				AccessKeyID: "key", SecretAccessKey: "dummy", SessionToken: "dummy",
+				Source: "Hard-coded credentials; values are irrelevant for local DynamoDB",
+			},
+		}),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	return dynamodb.NewFromConfig(cfg)
+}
