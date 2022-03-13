899ad9df2a67083d9bca586352450cbe9aa92d13
KantaHasegawa <gyoza20000@gmail.com>
2022-03-13 22:25:27 +0900

    feat: Add controller error handling

---
+++ src/errorHandler/controllerError.go
@@ -0,0 +1,11 @@
+package errorHandler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func ControllerError(err error, w *http.ResponseWriter){
+	fmt.Println(err.Error())
+	fmt.Fprint(*w, "sorry server error")
+}
